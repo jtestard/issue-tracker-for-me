9f3bad1fcfaa78dfe1aad29248cbf20d1b0c82cd
EC2 Default User <ec2-user@ip-172-31-17-55.us-west-2.compute.internal>
2016-05-23 01:46:55 +0000

    blah

--- launch.sh
+++ launch.sh
@@ -1,1 +1,1 @@
-tracd -s --port 8080 --basic-auth="*,/home/ec2-user/issue-tracker-for-me/.htpasswd,julestestard.com" /home/ec2-user/issue-tracker-for-me
+tracd -s --port 8080 --basic-auth="*,/home/ec2-user/issue-tracker-for-me/.htpasswd,julestestard.com" /home/ec2-user/issue-tracker-for-me &
